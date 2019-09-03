b4eac0e494671a3b4b11a8b22c1a587a584b1707
Anwesh43 <anweshthecool0@gmail.com>
2019-09-04 02:29:12 +0530

    adding  constants and libraries

--- colorsweeparcview/src/main/java/com/anwesh/uiprojects/colorsweeparcview/ColorSweepArcView.kt
+++ colorsweeparcview/src/main/java/com/anwesh/uiprojects/colorsweeparcview/ColorSweepArcView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.colorsweeparcview
 /**
  * Created by anweshmishra on 04/09/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("#1abc9c", "#9b59b6", "#e74c3c", "#3498db", "#e67e22")
+val scGap : Float = 0.01f
+val delay : Long = 30
+val rFactor : Float = 3f
+val triFactor : Float = 3f
+val triColor : Int = Color.WHITE
