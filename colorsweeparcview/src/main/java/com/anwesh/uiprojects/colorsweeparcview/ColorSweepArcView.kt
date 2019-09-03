0365d9baa6f287ffba477f8bc043b921b1ebd33c
Anwesh43 <anweshthecool0@gmail.com>
2019-09-04 02:38:29 +0530

    adding Canvas extension methods

--- colorsweeparcview/src/main/java/com/anwesh/uiprojects/colorsweeparcview/ColorSweepArcView.kt
+++ colorsweeparcview/src/main/java/com/anwesh/uiprojects/colorsweeparcview/ColorSweepArcView.kt
@@ -12,6 +12,7 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Path
 
 val colors : Array<String> = arrayOf("#1abc9c", "#9b59b6", "#e74c3c", "#3498db", "#e67e22")
 val scGap : Float = 0.01f
@@ -23,3 +24,38 @@ val triColor : Int = Color.WHITE
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))
+
+fun Canvas.drawTriangle(size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    paint.color = triColor
+    save()
+    scale(sc2, sc2)
+    val path : Path = Path()
+    path.moveTo(-size, size - 2 * size * sc1)
+    path.lineTo(0f, -size)
+    path.lineTo(size, size - 2 * size * sc1)
+    drawPath(path, paint)
+    restore()
+}
+
+fun Canvas.drawArc(i : Int, size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    paint.color = Color.parseColor(colors[i])
+    var sweepDeg : Float = 360f * (1 - sc1)
+    if (sc2 > 0f) {
+        sweepDeg = 360f * sc2
+    }
+    drawArc(RectF(-size, -size, size, size), 360f * sc1, sweepDeg, true, paint)
+}
+
+fun Canvas.drawCSANode(i : Int, scale : Float, sc : Float, paint : Paint) : Float {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / rFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(w / 2, h / 2)
+    drawArc(i, size, sc2, sc, paint)
+    drawTriangle(size, sc1, sc, paint)
+    restore()
+    return sc2
+}
