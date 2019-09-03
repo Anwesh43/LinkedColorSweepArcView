3696116c058132dba1785cf9169357e9b0c31c14
Anwesh43 <anweshthecool0@gmail.com>
2019-09-04 02:30:37 +0530

    adding Int and Float extesnsion methods

--- colorsweeparcview/src/main/java/com/anwesh/uiprojects/colorsweeparcview/ColorSweepArcView.kt
+++ colorsweeparcview/src/main/java/com/anwesh/uiprojects/colorsweeparcview/ColorSweepArcView.kt
@@ -19,3 +19,7 @@ val delay : Long = 30
 val rFactor : Float = 3f
 val triFactor : Float = 3f
 val triColor : Int = Color.WHITE
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))
