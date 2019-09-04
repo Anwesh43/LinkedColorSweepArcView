b70a4a05378e28aa46dcc003f792ba21023b0774
Anwesh43 <anweshthecool0@gmail.com>
2019-09-04 10:41:30 +0530

    adding ColorSweepArcView class

--- colorsweeparcview/src/main/java/com/anwesh/uiprojects/colorsweeparcview/ColorSweepArcView.kt
+++ colorsweeparcview/src/main/java/com/anwesh/uiprojects/colorsweeparcview/ColorSweepArcView.kt
@@ -59,3 +59,21 @@ fun Canvas.drawCSANode(i : Int, scale : Float, sc : Float, paint : Paint) : Floa
     restore()
     return sc2
 }
+
+class ColorSweepArcView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
