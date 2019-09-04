e8aadd4edb77eb670805dc43ba9fea08597a3e46
Anwesh43 <anweshthecool0@gmail.com>
2019-09-04 11:04:13 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedcolorsweeparcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcolorsweeparcview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedcolorsweeparcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.colorsweeparcview.ColorSweepArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ColorSweepArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
